e1a70b3ced92cb81b4ee7ae33dd18a759fc32393
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-06-26 22:39:40 +0530

    26th-july-problem_add-comments

--- balance-a-binary-search-tree.js
+++ balance-a-binary-search-tree.js
@@ -1,15 +1,24 @@
-var balanceBST = function (root) {
+//balance-a-binary-search-tree
+//26th-july-2024
+//binary-search-tree/recurrsion
+
+
+var balanceBST = function (root) {//function for inorder traversal
     const inOrder = (root) => {
-        if (!root) return [];
-        return [...inOrder(root.left), root.val, ...inOrder(root.right)];
+        if (!root) return [];// // base case: empty subtree returns an empty array
+        return [...inOrder(root.left), root.val, ...inOrder(root.right)];//combine left subtree values, current node value, and right subtree values
     };
-
+ //function to build a balanced BST from a sorted array
     const tree = (arr, l, r) => {
-        if (l >= r) return null;
-        const mid = Math.floor((l + r) / 2);
-        return new TreeNode(arr[mid], tree(arr, l, mid), tree(arr, mid + 1, r));
-    };
+        if (l >= r) return null;//base case: empty subarray returns null node
+        const mid = Math.floor((l + r) / 2);//find the middle index
+        return new TreeNode(arr[mid], //create a node with the middle element
+                        tree(arr, l, mid),  //recursively build left subtree
+                        tree(arr, mid + 1, r)); //recursively build right subtree
+  };
 
-    const sortedArr = inOrder(root);
-    return tree(sortedArr, 0, sortedArr.length);
+    const sortedArr = inOrder(root);  //get the in-order traversal of the input BST as a sorted array
+    return tree(sortedArr, 0, sortedArr.length);//build a balanced BST from the sorted array using the tree function
 };
+
+ 
