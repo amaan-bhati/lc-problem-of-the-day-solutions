1a55ca5e65646b6650537466b63d125cc22293f8
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-07-01 22:17:16 +0530

    1st-juky-problem_solution

---
+++ three-consecutive-odds.js
@@ -0,0 +1,15 @@
+var threeConsecutiveOdds = function (arr) {
+    let count = 0;
+    for (let num of arr) {
+        if (num % 2 !== 0) {
+            count++;
+            if (count === 3) {
+                return true;
+            }
+        } else {
+            count = 0;
+        }
+    }
+    return false;
+};
+                                                       
