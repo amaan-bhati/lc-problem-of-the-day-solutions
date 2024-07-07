ead634ab86f60af6ac9dc1cf82086f7e01b665d4
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-07-07 20:36:58 +0530

    6th-july-problem_solution

---
+++ pass-the-pillow.js
@@ -0,0 +1,15 @@
+//pass the pillow
+//6th-july-2024
+
+
+var numWaterBottles = function (numBottles, numExchange) {
+    let rem = 0;
+    let count = 0;
+    while (numBottles > 0) {
+        count += numBottles;
+        let num = numBottles + rem
+        numBottles = Math.floor((numBottles + rem) / numExchange)
+        rem = num % numExchange;
+    }
+    return count
+};
