c3b59cb1519fb290a389d623c943c297f62e3afb
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-07-09 21:12:19 +0530

    9th-july-problem_solution

---
+++ average-waiting-time.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} customers
+ * @return {number}
+ */
+var averageWaitingTime = function (customers) {
+  let total = 0;
+  let startTime = customers[0][0];
+  for (let customer of customers) {
+    const arriveTime = customer[0]; // the time customer arrive
+    const waitingTime = customer[1]; // the customer waiting time
+
+    if (startTime < arriveTime) {
+      // no customer until new customer finally came
+      startTime = arriveTime;
+    }
+
+    // add total waiting time
+    total += startTime + waitingTime - arriveTime;
+
+    // update the start time
+    startTime += waitingTime;
+  }
+
+  return total / customers.length;
+};
