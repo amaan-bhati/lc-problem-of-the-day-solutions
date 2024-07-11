495f15d718776f9bd7e9e01c26aba243f2e0f2cc
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-07-11 12:08:34 +0530

    11th-july-problem_add-comments-and-explanation

--- reverse-substrings-between-each-pair-of-parentheses.js
+++ reverse-substrings-between-each-pair-of-parentheses.js
@@ -1,10 +1,16 @@
+//Reverse substrings between each pair of parentheses
+//11th-july-2024
+//String, Stack
 var reverseParentheses = function (s) {
+    //find the innermost parentheses
     while (s.includes('(')) {
         let start = s.lastIndexOf('(');
         let end = s.indexOf(')', start);
+        //reverse the substring inside the parentheses
         let before = s.slice(0, start);
         let toReverse = s.slice(start + 1, end).split('').reverse().join('');
         let after = s.slice(end + 1);
+        //reconstruct the string without the innermost parentheses
         s = before + toReverse + after;
     }
     return s;
